36a6ef9873143c9d56a134e4e59b339b203e4db4
Greg Weng <snowmantw@gmail.com>
2013-11-26 03:56:14 +0800

    Update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,30 @@
-var fs = require('fs'),
-    path = require('path');
 
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-bower-task');
+  grunt.loadNpmTasks('grunt-gaia-builder');
   grunt.initConfig({
+    bower: {
+      install: {
+      },
+      // .bowerrc would NOT work.
+      options: {
+        cleanBowerDir: true,
+        targetDir: 'libs'
+      }
+    },
     jshint: {
       all: ['Gruntfile.js', '**/*.js']
+    },
+    gaiabuilder: {
+      options: {
+        depends: [],
+        targetDir: '/tmp/essential-build'
+      }
     }
   });
 
-  grunt.registerTask('build', function(){
-    // Do nothing. The gaia-essential should be a self-sustainable
-    // module, and can become other's base.
-  });
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('build', ['bower', 'gaiabuilder']);
+  grunt.registerTask('merge', ['bower']);
+  grunt.registerTask('default', ['bower']);
 }
