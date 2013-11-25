a312352588fa9cc050a9eaee0f4253b8114eb654
Greg Weng <snowmantw@gmail.com>
2013-11-26 02:53:04 +0800

    Update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,5 @@
+var fs = require('fs'),
+    path = require('path');
 
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
@@ -7,5 +9,30 @@ module.exports = function(grunt) {
     }
   });
 
+/*
+  grunt.registerTask('moveApps', 'Move apps inside gaia-essential to build.', function() {
+    var done = this.async();
+    var cwd = path.resolve(__dirname);
+    var apps = cwd + '/apps';
+    // We assume the whole Gaia apps would be put inside BUILD/apps,
+    // so we just need to move them to the upper directory.
+    fs.readdir(apps, function(err, files){
+      if (err)
+        throw err;
+      files.forEach(function(fn, i, a){
+        fs.rename(apps+'/'+fn, cwd+'/../'+fn, function(err){
+          if (err)
+            throw err;
+          done();
+        });
+      });
+    });
+  });
+
+  grunt.registerTask('build', ['moveApps']);
+*/
+  grunt.registerTask('build', function(){
+    // Do nothing.
+  });
   grunt.registerTask('default', ['jshint']);
 }
